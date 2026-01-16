104f781499abd97ddf9ade7a700f8836a7b2de80
Saurav_Prakash <166358805+Sauravprakash9095@users.noreply.github.com>
2026-01-16 18:56:34 +0000

    2.js

---
+++ js/2.js
@@ -0,0 +1,10 @@
+const accountId ="123456"
+let accountHolderName ="Saurav"
+var accountPassowrd ="234566"
+accountCity="Indore"
+
+accountHolderName ="Prince"
+accountPassowrd ="0987654"
+accountCity ="Patna"
+
+console.table([accountId,accountHolderName, accountPassowrd,accountCity]);
