51d60085d21c31281cbcc8157141edf2b57459c6
Saurav_Prakash <166358805+Sauravprakash9095@users.noreply.github.com>
2026-01-16 19:15:58 +0000

    Datatypes

---
+++ js/datatypes.js
@@ -0,0 +1,16 @@
+const Id ="12453"
+let age = 13
+let state
+let city = undefined
+let temperature = null
+console.log(typeof city)
+console.log(typeof age)
+console.log(typeof Id)
+console.log(typeof state)
+console.log(typeof temperature) // Null ka output object hota hai
+
+
+console.log(typeof "state");
+
+
+console.log(age);
